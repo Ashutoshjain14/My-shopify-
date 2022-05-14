be4e0d160539c466ed6c010951ff204e160ad35c
Ashwatthama14 <0902au191015@rjit.ac.in>
2022-05-14 16:43:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import { Fragment } from "react";
 
-function App() {}
+function App() {
+  <Fragment>
+    <Header />
+    <Banner />
+  </Fragment>;
+}
 
 export default App;
