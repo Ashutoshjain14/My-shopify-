9617aeaa85347189a556ec7d673bafcd8435a776
Ashwatthama14 <0902au191015@rjit.ac.in>
2022-05-14 16:39:58 +0530

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function Header() {
+  return (
+    <Fragment>
+      <h1>The easiest way to sell online in India</h1>
+    </Fragment>
+  );
+}
+
+export default Header;
